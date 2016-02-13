c46779f1f761fcf7e03d996068fbcb006c3a715c
Mart Mangus <mamangus@enos.itcollege.ee>
2016-02-13 18:29:15 +0200

    Pealeht ja esimene praktikum lisatud.

---
+++ index.php
@@ -0,0 +1,89 @@
+<!doctype html>
+<html lang="et">
+<head>
+  <meta charset="utf-8">
+  <title>I244 kaugõpe</title>
+</head>
+<body>
+<h1>Võrgurakendused I <small>kaugõpe 2016 kevadsemester</small></h1>
+<h2>Auditoorne õppetöö</h2>
+<h3>Loengud</h3>
+<p>Siia tulevad viited loengute materjalidele</p>
+<h3>Praktikumid</h3>
+<p><a href="praktikum1.php">Praktikum 1 (2016.02.14)</a></p>
+<h2>Iseseisev töö</h2>
+<h3>Iganädalased kodused tööd</h3>
+<p>
+  Iga nädal tuleb iseseisvalt läbi töötada välja toodud teemad ning tehtud töö kohta saata aruanne. <a target="_blank" href="http://goo.gl/forms/SZ5ngxlN4m">Aruande saatmise vorm</a>.<br />
+  Iga (korrektne) aruanne annab 2 punkti ja kokku moodustavad need aruanded 32% aine tulemusest.<br />
+  Kui viidatud materjalidest jääb väheks, siis uuri ja otsi ise internetist juurde.<br />
+  Võid vaadata ka päevaõppe paraktikumide salvestusi, mis leiad <a href="https://echo360.e-ope.ee/ess/portal/section/88b1a22f-7260-4f6e-823c-451034853ea6">kuskilt siit</a>.
+</p>
+<table>
+  <tr>
+    <th>Nr</th><th>Teema</th><th>Päevaõppe materjalid</th><th>Tähtaeg</th>
+  </tr><tr>
+    <td>1</td><td>Sissejuhatus HTML-i</td>
+    <td>
+      <a href="https://echo360.e-ope.ee/ess/echo/presentation/7e9c8258-3664-4b9e-b487-dfbae7c5dcf9?ec=true">loeng</a>
+      <a href="http://enos.itcollege.ee/~ttanav/VRI/Praktikumid/prax01/ylesanne.html">praktikum</a>
+      <a href="http://enos.itcollege.ee/~ttanav/VRI/Kodused/01/ylesanne.html">ülesanne</a>
+    </td><td>21.02.2016 23:59</td>
+  </tr><tr>
+    <td>2</td><td>HTML-i keerukamad osad</td>
+    <td>
+      <a href="https://echo360.e-ope.ee/ess/echo/presentation/bdbe400c-1d81-4e59-bf28-32dccc26af78?ec=true">loeng</a>
+      <a href="http://enos.itcollege.ee/~ttanav/VRI/Praktikumid/prax02/ylesanne.html">praktikum</a>
+      <a href="http://enos.itcollege.ee/~ttanav/VRI/Kodused/02/ylesanne.html">ülesanne</a>
+    </td>
+    <td>28.02.2016 23:59</td>
+  </tr><tr>
+    <td>3</td><td>CSS sissejuhatus</td><td>...</td><td>06.03.2016 23:59</td>
+  </tr><tr>
+    <td>4</td><td>Kujundus ja paigutus</td><td>...</td><td>13.03.2016 23:59</td>
+  </tr><tr>
+    <td>5</td><td>Javascript</td><td>...</td><td>20.03.2016 23:59</td>
+  </tr><tr>
+    <td>6</td><td>jQuery</td><td>...</td><td>27.03.2016 23:59</td>
+  </tr><tr>
+    <td>7</td><td>PHP sissejuhatus</td><td>...</td><td>03.04.2016 23:59</td>
+  </tr><tr>
+    <td>8</td><td>Andmeside (GET ja POST)</td><td>...</td><td>10.04.2016 23:59</td>
+  </tr><tr>
+    <td>9</td><td>MVC</td><td>...</td><td>17.04.2016 23:59</td>
+  </tr><tr>
+    <td>10</td><td>Autentimine, küpsised, sessioonid</td><td>...</td><td>24.04.2016 23:59</td>
+  </tr><tr>
+    <td>11</td><td>MySQL</td><td>...</td><td>01.05.2016 23:59</td>
+  </tr><tr>
+    <td>12</td><td>MySQL + PHP</td><td>...</td><td>08.05.2016 23:59</td>
+  </tr><tr>
+    <td>13</td><td>MySQL + PHP (vol 2)</td><td>...</td><td>15.05.2016 23:59</td>
+  </tr><tr>
+    <td>14</td><td>Turvalisus</td><td>...</td><td>22.05.2016 23:59</td>
+  </tr><tr>
+    <td>15</td><td>...</td><td>...</td><td>29.05.2016 23:59</td>
+  </tr><tr>
+    <td>16</td><td>...</td><td>...</td><td>05.06.2016 23:59</td>
+  </tr>
+</table>
+
+<h3>Kodune projekt</h3>
+<p>Täpsemad nõudmised projektile tulevad siia semestri lõpu poole.</p>
+<h4>Oluline info edasijõudnutele</h4>
+<p>
+  Kes tunneb, et valdab juba praegu teemat piisavalt võib esitada kodutöö 31.-ks märtsiks 2016.<br />
+  Enneaegselt esitatud (asjaliku) töö eest saab topelt punktid ning see võimaldab hea hinde saada ka lihtsamaid iganädalaseid aruandeid tegemata.<br />
+  Kui Sa ei ole kindel, kas valdad teemat piisavalt hästi, siis tee siiski iganädalaseid töid ja esita kodune projekt õigel ajal.
+</p>
+
+
+<h3>Jututuba</h3>
+<p>
+  Kui tegeled aine teemadega, siis ilmuta end ka <a href="https://chatlink.com/#i244">jututoas</a>!<br />
+  Saab kaastudengitega suhelda ja abi küsida. Kui väga läbuks ei lähe, siis jälgib seda ka õppejõud ning püüab abistada.<br />
+  Kui keegi tudeng on jututoas aktiivne abistaja, siis saab selle eest ka lisapunkte!
+</p>
+
+</body>
+</html>
