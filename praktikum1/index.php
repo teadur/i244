f6868c417f40f10d10e2817e8778aeb2205c8b41
Mart Mangus <mart@mangus.ee>
2016-04-08 12:39:40 +0300

    Praktikum1 indeks ka

---
+++ praktikum1/index.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="et">
+<head>
+  <meta charset="utf-8">
+  <title>I244 kaugõpe: 1. praktikum</title>
+</head>
+<body>
+<h1>I244 esimene praktikum</h1>
+<p>
+  Tänases praktikumis püüame tööle panna kõik vajaliku, et saaks kodus iseseisva tööga jätkata.<br />
+  Liigume teemadega edasi väga kiiresti, kodus päevaõppe materjalide abiga saad pikemalt süveneda ja sisulisemalt õppida.<br />
+  Kui jääd hätta, siis otsi abi internetist, küsi kaastudengitelt ja tülita õppejõudu.
+</p>
+
+<h2>Abimaterjalid</h2>
+<p>
+  <a href="https://moodle.hitsa.ee/mod/page/view.php?id=430425">Kuidas kooli serveri (enos) failidele ja MySQL andmebaasile ligipääseda?</a>
+</p>
+
+<h2 id="need">Ülesanded</h2>
+<ol>
+  <li>Loo oma arvutis lihtne <em>HTML</em> lehekülg. Lisa lehele ka mõni pilt.</li>
+  <li>Laadi leht ülesse <em>enos.itcollege.ee</em> serverisse.</li>
+  <li>Lisa lehele <em>CSS</em> fail ja muuda loodud lehekülg ilusamaks.</li>
+  <li>Tee lehele midagi huvitavat kasutades <em>Javascript</em>-i. Näiteks tee kell, mis loendab, mitu sekundit on praktikumi lõpuni vms :)</li>
+  <li>Võta kasutsele <em>PHP</em>. Muuda on <em>HTML</em> fail <em>PHP</em> failiks ning loo lehele loendur, et mitu inimest on lehel käinud. Andmed võid salvestada lihtsasse tekstifaili.</li>
+  <li>Logi sisse <em>MySQL</em> serverisse, seal loo tabel külastajate IP-aadressite salvestamiseks.</li>
+  <li>Täienda oma PHP faili nii, et see salvestaks külastajate IP-aadresse ning näitaks ka välja statistikat vms.</li>
+  <li>Pane kogu oma kirjutatud kood versioonihaldusesse. (Väga oluline punkt!)</li>
+</ol>
+
+<?php require('../lisaylesanded.tpl') ?>
+
+</body>
+</html>
