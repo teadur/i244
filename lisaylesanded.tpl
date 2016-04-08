635fcf0f0055f4e4c23d544ed7111c20bbbd6dc8
Mart Mangus <mart@mangus.ee>
2016-04-08 12:38:24 +0300

    Praktikum2 lisatud

---
+++ lisaylesanded.tpl
@@ -0,0 +1,7 @@
+<h3>Lisaülesanded</h3>
+<p>Kui said kõik valmis ja jäi veel aega ülegi, siis siin mõned lisapunktide teenimise võimalused (kehtivad ka väljaspool praktikumi aega):</p>
+<ul>
+  <li>Tee see käesolev praktikumi leht ilusamaks ning <em>pull request</em> õppejõule.</li>
+  <li>Kujunda aine pealehte ning <em>pull request</em> õppejõule.</li>
+  <li>Kirjuta aine pealehele "iganädalaste kodutööde" juurde taimer, mis näitab, palju aega on jäänud aruande esitamiseks.</li>
+</ul>
---
+++ praktikum2/index.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="et">
+<head>
+  <meta charset="utf-8">
+  <title>I244 kaugõpe: 2. praktikum</title>
+</head>
+<body>
+<h1>I244 teine praktikum</h1>
+<p>
+  Tänases praktikumis lõpetame eelmise praktikumi töö (et tekiks ülevaade kõikidest tehnoloogiastest mis kasutame) ning proovime lähemalt tuttavaks saada <em>CSS</em>-i ja <em>Javascript</em>-iga.
+</p>
+<p>
+  Teeme paarisprogrammeerimist. See tähendab, et ühe arvuti taga on 2 tudengit ja kordamööda (~15 minutiliste vahedega) kirjutate koodi.</br >
+</p>
+<h2>Ülesanded</h2>
+<ol>
+  <li>Tee lõpuni kõik <a href="../praktikum1/#need">eelmise praktikumi ülesanded</a>.</li>
+  <li>Veendu, et oled kasutanud <em>HTML</em>-i, <em>CSS</em>-i, <em>Javascript</em>-i, <em>PHP</em>-d, <em>MySQL</em>-i ja <em>Git</em>-i.</li>
+  <li>Pane <em>HTML</em>-i, <em>CSS</em>-i, <em>Javascript</em>-i ja <em>PHP</em> kõik eraldi failidesse ning vajadusel ka eraldi kataloogi.</li>
+  <li>Saame CSS-ga rohkem tutavamaks: teeme oma lehele CSS-ga animatsiooni!</li>
+  <li>Mõtle välja ja realiseeri <em>Javascript</em>-iga lihtne mäng <small>(näiteks ussimäng, mingi klikkimise mäng või midagi sellist)</small>.
+    <br />Alusta millestki väga lihtsast, võimalusel seo <em>CSS</em> animatsioonid <em>Javascript</em>-i mänguga.
+  </li>
+</ol>
+
+<?php require('../lisaylesanded.tpl') ?>
+
+</body>
+</html>
