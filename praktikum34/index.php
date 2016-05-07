a0ab1638f3631f1f06d60b5b1d6b72c44df9ca07
Mart Mangus <mart@mangus.ee>
2016-05-07 12:15:27 +0300

    Praktikum 3 ja 4 lisatud.

---
+++ praktikum34/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="et"><head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+  <meta charset="utf-8">
+  <title>I244 kaugõpe: 3 ja 4. praktikum</title>
+</head>
+<body>
+
+<h1>I244 kolmas ja neljas praktikum</h1>
+<p>
+  Tänases ja homses praktikumis teeme täitsa valmis ühe lihtsama veebirakenduse.
+</p>
+<p>
+  Soovitatav on töötada paaris ning teha paarisprogrammeerimist.
+</p>
+<h2>Ülesanded</h2>
+<ol>
+  <li>Veenduge, et olete ära teinud kõik <a href="http://enos.itcollege.ee/%7Emamangus/i244/praktikum1/#need">esimese praktikumi ülesanded</a>.</li>
+  <li>Vaadake üle, et töö oleks versioonihalduses ja loogiliselt struktureeritud (erinevad teemad eraldi failides ja vajadusel ka kataloogides).</li>
+  <li>
+    Arutlege koos üle, et mis on:
+    <ul>
+      <li>HTML,</li><li>CSS,</li><li>Javascript,</li><li>PHP,</li><li>andmebaasi struktuur,</li><li>andmebaasi päringud,</li><li>andmed andmebaasis.</li>
+    </ul>
+    Kui mõni teema on segane, siis uurige koos juurde, küsige õppejõult ja tehke see selgeks.
+  </li>
+  <li>
+    Mõelge välja lihtne rakendus, mille koos valmis teete.<br />
+    Näiteks foorum, pildigalerii, failihoidla, mäng vms. Rakenduse kasutajal peab olema võimalus midagi lehele postitada. Ideaalsel juhul võiks saada lisada ka faile.
+  </li>
+  <li>Võtke uue rakenduse aluseks eelmistes praktikumides tehtud leht ja hakake seda täiendama. Säilitage sealne kasutajate IP-aadresseid salvestav funktsionaalsus.</li>
+  <li>Tehke valmis uue rakenduse HTML struktuur ja vormid.</li>
+  <li>Muutke CSS-i kasutades rakenduse välimus nii viisakaks kui võimalik.</li>
+  <li>Looge rakenduse tarbeks andmebaasi struktuur.</li>
+  <li>Siduge PHP-d kasutades loodud kasutajaliides ja andmebaas kokku, nii et HTML vormide kaudu saaks andmebaasi infot salvestada.</li>
+  <li>
+    Näidake oma rakendust eelinfot andmata kaastudengitele.
+    <ul>
+      <li>Kas rakenduse kasutajaliidesest on intuitiivselt aru saada, mis rakendusega on tegu ja kuidas seda kasutada?</li>
+      <li>Kas lehte on lihtne ja mugav kasutada?</li>
+    </ul>
+    Kasutage kõiki võimalikke vahendeid (CSS, Javascript jne), et eelnevaid punkte parendada.
+</ol>
+
+<h3>Lisaülesanded</h3>
+<p>Kui said kõik valmis ja jäi veel aega ülegi, siis siin mõned 
+lisapunktide teenimise võimalused (kehtivad ka väljaspool praktikumi 
+aega):</p>
+<ul>
+  <li>Tee see käesolev praktikumi leht ilusamaks ning <em>pull request</em> õppejõule.</li>
+  <li>Kujunda aine pealehte ning <em>pull request</em> õppejõule.</li>
+  <li>Kirjuta aine pealehele "iganädalaste kodutööde" juurde taimer, mis näitab, palju aega on jäänud aruande esitamiseks.</li>
+</ul>
+
+
+</body></html>
