6608652a1c9da3c1fed987aeefce763dbc417975
Mart Mangus <mart@mangus.ee>
2016-05-28 03:04:56 +0300

    Elementaarne XSS kaitse

--- praktikum5/salvesta.php
+++ praktikum5/salvesta.php
@@ -3,7 +3,7 @@
   if (count($_POST)) {
     $html = sprintf(
       '<tr><td>%s</td><td><a href="%s">%s</a></td><td><a href="%s">%s</a></td></tr>' . "\n",
-      $_POST['nimi'], $_POST['git'], $_POST['git'], $_POST['link'], $_POST['link']
+      htmlspecialchars($_POST['nimi']), $_POST['git'], $_POST['git'], $_POST['link'], $_POST['link']
     );
     file_put_contents('esitatud.html', $html, FILE_APPEND);
   }
